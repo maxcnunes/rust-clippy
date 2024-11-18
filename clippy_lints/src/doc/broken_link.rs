bbfa2f248b1784cac0f95862f65e624f01ccadd8
Max Claus Nunes <maxcnunes@gmail.com>
2024-11-18 16:09:53 -0300

    Include reason why a link is considered broken

--- clippy_lints/src/doc/broken_link.rs
+++ clippy_lints/src/doc/broken_link.rs
@@ -6,17 +6,40 @@ use rustc_span::{BytePos, Span};
 use super::DOC_BROKEN_LINK;
 
 pub fn check(cx: &LateContext<'_>, attrs: &[Attribute]) {
-    for span in BrokenLinkLoader::collect_spans_broken_link(attrs) {
-        span_lint(cx, DOC_BROKEN_LINK, span, "possible broken doc link");
+    for broken_link in BrokenLinkLoader::collect_spans_broken_link(attrs) {
+        let reason_msg = match broken_link.reason {
+            BrokenLinkReason::MultipleLines => "broken across multiple lines",
+            BrokenLinkReason::MissingCloseParenthesis => "missing close parenthesis",
+            BrokenLinkReason::WhiteSpace => "whitespace within url",
+        };
+
+        span_lint(
+            cx,
+            DOC_BROKEN_LINK,
+            broken_link.span,
+            format!("possible broken doc link: {reason_msg}"),
+        );
     }
+}
+
+/// The reason why a link is considered broken.
+enum BrokenLinkReason {
+    MultipleLines,
+    MissingCloseParenthesis,
+    WhiteSpace,
+}
+
+/// Broken link data.
+struct BrokenLink {
+    reason: BrokenLinkReason,
+    span: Span,
 }
 
 /// Scan AST attributes looking up in doc comments for broken links
 /// which rustdoc won't be able to properly create link tags later.
-#[derive(Debug)]
 struct BrokenLinkLoader {
     /// List of spans for detected broken links.
-    spans_broken_link: Vec<Span>,
+    broken_links: Vec<BrokenLink>,
 
     /// Mark it has detected a link and it is processing whether
     /// or not it is broken.
@@ -51,9 +74,9 @@ struct BrokenLinkLoader {
 
 impl BrokenLinkLoader {
     /// Return spans of broken links.
-    fn collect_spans_broken_link(attrs: &[Attribute]) -> Vec<Span> {
+    fn collect_spans_broken_link(attrs: &[Attribute]) -> Vec<BrokenLink> {
         let mut loader = BrokenLinkLoader {
-            spans_broken_link: vec![],
+            broken_links: vec![],
             active: false,
             active_pos_start: 0,
             active_span: None,
@@ -65,7 +88,7 @@ impl BrokenLinkLoader {
             start: 0_u32,
         };
         loader.scan_attrs(attrs);
-        loader.spans_broken_link
+        loader.broken_links
     }
 
     fn scan_attrs(&mut self, attrs: &[Attribute]) {
@@ -79,7 +102,7 @@ impl BrokenLinkLoader {
                 if idx > 0 && self.active && self.processing_link_url {
                     let prev_attr = &attrs[idx - 1];
                     let prev_end_line = prev_attr.span.hi().0;
-                    self.record_broken_link(prev_end_line);
+                    self.record_broken_link(prev_end_line, BrokenLinkReason::MissingCloseParenthesis);
                 }
                 self.reset_lookup();
             }
@@ -87,7 +110,7 @@ impl BrokenLinkLoader {
 
         if self.active && self.processing_link_url {
             let last_end_line = attrs.last().unwrap().span.hi().0;
-            self.record_broken_link(last_end_line);
+            self.record_broken_link(last_end_line, BrokenLinkReason::MissingCloseParenthesis);
             self.reset_lookup();
         }
     }
@@ -139,7 +162,7 @@ impl BrokenLinkLoader {
                 if self.url_multiple_lines {
                     // +3 skips the opening delimiter and +1 to include the closing parethesis
                     let pos_end = attr_span.lo().0 + u32::try_from(pos).unwrap() + 4;
-                    self.record_broken_link(pos_end);
+                    self.record_broken_link(pos_end, BrokenLinkReason::MultipleLines);
                     self.reset_lookup();
                 }
                 self.reset_lookup();
@@ -148,7 +171,7 @@ impl BrokenLinkLoader {
 
             if self.reading_link_url && c.is_whitespace() {
                 let pos_end = u32::try_from(pos).unwrap();
-                self.record_broken_link(pos_end);
+                self.record_broken_link(pos_end, BrokenLinkReason::WhiteSpace);
                 self.reset_lookup();
                 continue;
             }
@@ -173,14 +196,14 @@ impl BrokenLinkLoader {
         self.url_multiple_lines = false;
     }
 
-    fn record_broken_link(&mut self, pos_end: u32) {
+    fn record_broken_link(&mut self, pos_end: u32, reason: BrokenLinkReason) {
         if let Some(attr_span) = self.active_span {
             let start = BytePos(self.active_pos_start);
             let end = BytePos(pos_end);
 
-            let com_span = Span::new(start, end, attr_span.ctxt(), attr_span.parent());
+            let span = Span::new(start, end, attr_span.ctxt(), attr_span.parent());
 
-            self.spans_broken_link.push(com_span);
+            self.broken_links.push(BrokenLink { reason, span });
         }
     }
 }
